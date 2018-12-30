67c39391d1bc3d05a556a59c587d2c5b7ca070c7
J-H-Freedman <37190247+J-H-Freedman@users.noreply.github.com>
2018-12-30 16:23:41 -0500

    Update dice.py
    Figured out how to get return values to work

--- dice.py
+++ dice.py
@@ -1,51 +1,40 @@
+# Simple dice roller using a series of functions.
+
 import random
 
-# Most of this stuff is commented out.  
-# Part of the reason is through an eXtreMe programming approach and I wanted to preserve my thought process which currently is defeloped for functions.
-# Part of it is also that I apperently don't kow how the 'return' script works between functions.
-# So preserving this part of the code may also aid in showing progress as a programmer once I'm more fluent in the logic.
-# I'd like to return to this code one day to make it work through functions.
-# Until then, copy-pasting everything into the Main() function seems to work just fine.
+# Get values
+def GetDieTypeFromUser():
+	return int(input("How many sides to the die? >"))
+	
+def GetNumberOfDiceFromUser():
+	return int(input("How many dice are being rolled? >"))
 
-#def DefineDieType():
-#	die_sides = int(input("How many sides to the die? >"))
-#	return die_sides
-#
-#def RollDie():
-#	roll = random.randint(1, die_sides)
-#	return roll
-#
-#def RollMultipleDice():
-#	number_of_dice = int(input("How many dice are being rolled? >"))
-#	dice_rolled = 0
-#
-#	global total_from_dice
-#
-#	while dice_rolled < number_of_dice:
-#		RollDie()
-#		total_from_dice += roll
-#		dice_rolled +=1
-#
-#def AddModifier():
-#	modifier = int(input("What is the modifier? >"))
-#	global total_from_dice
-#	total_from_dice += modifier
+def GetModifierFromUser():
+	return int(input("What is the modifier? >"))
+
+# Roll dice
+def RollMultipleDice(number_of_dice, die_sides):
+	dice_rolled = 0
+	total_from_dice = 0
+
+	while dice_rolled < number_of_dice:
+		total_from_dice += random.randint(1, die_sides)
+		dice_rolled += 1
+
+	return total_from_dice	
 
 def Main():
+	# Lasts until crash.  Type a non-int to crash deliberately.
 	while True:
-		total_from_dice = 0
-
-		die_sides = int(input("How many sides to the die? >"))
-		number_of_dice = int(input("How many dice are being rolled? >"))
-		dice_rolled = 0
-
-		while dice_rolled < number_of_dice:
-			total_from_dice += random.randint(1, die_sides)
-			dice_rolled +=1
-
-		modifier = int(input("What is the modifier? >"))
+		# Get values
+		die_sides = GetDieTypeFromUser()
+		number_of_dice = GetNumberOfDiceFromUser()
+		modifier = GetModifierFromUser()
+		
+		# Calculate and print
+		total_from_dice = RollMultipleDice(number_of_dice, die_sides)
+		print("... \n Rolling ", number_of_dice, "d", die_sides, ": \n", total_from_dice, "\n \n", total_from_dice, "+ Modifier(", modifier, ") :")
 		total_from_dice += modifier
-
-		print("\n Result: ", total_from_dice)
+		print("--->", total_from_dice, "<--- \n ", "...")
 
 Main()
